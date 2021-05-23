b92be8ea9923977f82feb8e9353c92a4cc934d1a
Delahayes Theo <delahayest@gmail.com>
2021-05-23 17:57:51 +0200

    Chapter 10: Twig

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -42,8 +42,7 @@ class CommentCrudController extends AbstractCrudController
         yield EmailField::new('email');
         yield TextareaField::new('text')
             ->hideOnIndex();
-        yield TextField::new('photoFilename')
-            ->onlyOnIndex();
+        yield TextField::new('photoFilename');
 
         $createdAt = DateTimeField::new('createdAt')->setFormTypeOptions([
             'html5' => true,
