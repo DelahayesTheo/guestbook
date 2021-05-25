8046cdb06707cd1a10c39215d3653d90914bfff8
Delahayes Theo <delahayest@gmail.com>
2021-05-25 13:33:07 +0200

    Chapter 14: Forms

--- migrations/Version20210525110030.php
+++ migrations/Version20210525110030.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210524192952 extends AbstractMigration
+final class Version20210525110030 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,13 +20,19 @@ final class Version20210524192952 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_911533C8989D9B62 ON conference (slug)');
+        $this->addSql('
+            CREATE TABLE sessions (
+                sess_id VARCHAR(128) NOT NULL PRIMARY KEY,
+                sess_data BYTEA NOT NULL,
+                sess_lifetime INTEGER NOT NULL,
+                sess_time INTEGER NOT NULL
+            )
+        ');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP INDEX UNIQ_911533C8989D9B62');
     }
 }
